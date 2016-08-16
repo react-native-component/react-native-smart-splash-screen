a221f120cdbaa3a1e8bf868276f7246997c352f3
cyqresig <moonsunfall@aliyun.com>
2016-08-16 15:11:32 +0800

    added bitmap transform

--- android/src/main/java/com/reactnativecomponent/splashscreen/RCTSplashScreen.java
+++ android/src/main/java/com/reactnativecomponent/splashscreen/RCTSplashScreen.java
@@ -3,9 +3,13 @@ package com.reactnativecomponent.splashscreen;
 import android.app.Activity;
 import android.app.Dialog;
 import android.content.Context;
+import android.content.res.Resources;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Color;
+import android.graphics.Matrix;
 import android.os.Handler;
-import android.view.Display;
+import android.util.DisplayMetrics;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.ViewGroup.LayoutParams;
@@ -20,8 +24,6 @@ import android.widget.LinearLayout;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
-
-import java.lang.reflect.Array;
 
 
 public class RCTSplashScreen extends ReactContextBaseJavaModule {
@@ -108,6 +110,60 @@ public class RCTSplashScreen extends ReactContextBaseJavaModule {
         return drawableId;
     }
 
+    private int getStatusBarHeight() {
+        int height = 0;
+        Resources resources = getActivity().getResources();
+        int resourceId = resources.getIdentifier("status_bar_height", "dimen", "android");
+        if (resourceId > 0) {
+            height = resources.getDimensionPixelSize(resourceId);
+        }
+        return height;
+    }
+
+    private int getNavigationBarHeight() {
+        int height = 0;
+        Resources resources = getActivity().getResources();
+        int resourceId = resources.getIdentifier("navigation_bar_height", "dimen", "android");
+        if (resourceId > 0) {
+            height = resources.getDimensionPixelSize(resourceId);
+        }
+        return height;
+    }
+
+    private Bitmap getClipBitmap(int drawableId) {
+
+        Resources resources = getActivity().getResources();
+        DisplayMetrics dm = resources.getDisplayMetrics();
+        int screenWidth = dm.widthPixels;
+        int screenHeight = dm.heightPixels;
+
+        Bitmap bitmap = BitmapFactory.decodeResource(getActivity().getResources(), drawableId);
+
+        int navigationBarHeight = getNavigationBarHeight();
+        int statusBarHeight = getStatusBarHeight();
+        int width = bitmap.getWidth();
+        int height = bitmap.getHeight();
+        int newWidth = screenWidth;
+        int newHeight = screenHeight + navigationBarHeight;
+
+        float scaleWidth = ((float) newWidth) / width;
+        float scaleHeight = ((float) newHeight) / height;
+        Matrix matrix = new Matrix();
+        matrix.postScale(scaleWidth, scaleHeight);
+        Bitmap resizedBitmap = Bitmap.createBitmap(bitmap, 0, 0, width,
+                height, matrix, true);
+        bitmap.recycle();
+
+        int y = statusBarHeight;
+        int x = 0;
+
+        bitmap = Bitmap.createBitmap(resizedBitmap, x, y, resizedBitmap.getWidth(),
+                resizedBitmap.getHeight() - y - navigationBarHeight, null, false);
+        resizedBitmap.recycle();
+        return bitmap;
+    }
+
+
     private void openSplashScreen() {
         final int drawableId = getImageId();
         if ((dialog != null && dialog.isShowing())||(drawableId == 0)) {
@@ -115,15 +171,13 @@ public class RCTSplashScreen extends ReactContextBaseJavaModule {
         }
         getActivity().runOnUiThread(new Runnable() {
             public void run() {
-                Display display = getActivity().getWindowManager().getDefaultDisplay();
                 Context context = getActivity();
 
                 imageView = new ImageView(context);
-                imageView.setImageResource(drawableId);
+                imageView.setImageBitmap(getClipBitmap(drawableId));
                 LayoutParams layoutParams = new LinearLayout.LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT);
                 imageView.setLayoutParams(layoutParams);
-                imageView.setMinimumHeight(display.getHeight());
-                imageView.setMinimumWidth(display.getWidth());
+
                 imageView.setBackgroundColor(Color.BLACK);
                 imageView.setScaleType(ImageView.ScaleType.FIT_XY);
 
@@ -134,7 +188,7 @@ public class RCTSplashScreen extends ReactContextBaseJavaModule {
                             WindowManager.LayoutParams.FLAG_FULLSCREEN);
                 }
                 dialog.setContentView(imageView);
-//                dialog.setCancelable(false);
+                dialog.setCancelable(false);
                 dialog.show();
             }
         });
